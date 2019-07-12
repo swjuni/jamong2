6a34413a51b1a94885a3175b8e17f62cc8d3cbd0
juyoung <wanke0524@gmail.com>
2019-07-13 00:44:18 +0900

    카테고리 인서트 많이

---
+++ src/main/webapp/dbsql/카테고리ML insert.sql
@@ -0,0 +1,161 @@
+select * from category_l;insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'디자인');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'IT');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'콘텐츠 제작');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'마케팅');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'번역.통역');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'문서.취업');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'비즈니스 컨설팅');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'레슨');
+insert into category_l(category_no,category_name_l)
+values(category_l_seq.nextval,'간판.인쇄');
+
+select * from category_m;
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'3D 모델링','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'배너','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'웹.모바일 디자인','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'PPT','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'캘리그라피','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'북커버','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'일러스트','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'포토샵','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'현수막','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,1,'의류디자인','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'워드프레스','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'웹사이트 개발','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'쇼핑몰','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'모바일앱.웹','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'프로그램 개발','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'게임','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'데이터베이스','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'블록체인','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'서버 및 기술 지원','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'QA테스트','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,3,'영상','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'사진','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,2,'더빙','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,3,'3음악','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,3,'엔터테이너','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,3,'기타','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'종합광고대행','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'블로그.카페','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'유튜버','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'인스타 인플루언서','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'SNS마케칭','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'키워드 배너광고','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'쇼핑몽.스토어','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'앱마케팅','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'언론홍보','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,4,'기타','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,5,'번역','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,5,'통역','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,5,'영상번역','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'면접.취업 코칭','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'자소서.이력서','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'카피라이팅','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'논문','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'글작성.대본','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'타이핑','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,6,'기타','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'사업계획','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'법률.법무','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'세무회계','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'창업.경영진단','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'리서치.서베이','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'해외사업','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'HR인사','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,7,'기타','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'외국어','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'컴퓨터','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'마케팅','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'커뮤니케이션','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'영상.사진','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'스포츠','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'음악.악기','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'취미.문화','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'기타','설명');
+
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,9,'인쇄','설명');
+insert into category_m(category_no_m,category_no_l,category_name,category_desc)
+values(category_m_seq.nextval,8,'간판','설명');
+
+commit;
+
